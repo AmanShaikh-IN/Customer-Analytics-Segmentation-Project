ad9f427e9efe981d9318d3ffdf9dcaf5398e841a
AmanShaikh <132216790+AmanShaikh-IN@users.noreply.github.com>
2025-01-14 21:32:25 +0530

    Update customer_analytics_segmentation_project.py
    Added Problem Statement Description

--- customer_analytics_segmentation_project.py
+++ customer_analytics_segmentation_project.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 
+'''
+Case Study
+
+To what extent does our platform’s acquisition channel influence the learning outcomes of our students?
+Are there any geographical locations where most of our students discover the platform, specifically through social media platforms like YouTube or Facebook?
+
+You will work on real-world customer data to perform market segmentation—crucial for businesses to understand customer behavior and improve marketing efficiency. The project will involve data preprocessing,
+exploratory data analysis (EDA), feature engineering, implementation of clustering algorithms, and interpretation of results. You’ll use two popular clustering techniques: k-means and hierarchical 
+clustering.
+'''
 # Customer Analytics Segmentation Project
 import numpy as np
 import pandas as pd
